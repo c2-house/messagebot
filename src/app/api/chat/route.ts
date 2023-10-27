b39c11cf11c0024cbbd43be545f14498699b8dbf
eunchae <kec0130@gmail.com>
2023-10-27 17:19:44 +0900

    feat: add openai route handler

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,39 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  const { relation, name, reason, manner } = await req.json();
+
+  const basePrompts = process.env.BASE_PROMPT!.split('\\n');
+
+  const prompt = `${basePrompts[0]}
+1. ${basePrompts[1]} ${relation}.${name && ` ${name} ${process.env.NAME_PROMPT!}`}
+2. ${basePrompts[2]} ${reason}.
+3. ${basePrompts[3]} ${manner}.
+4. ${basePrompts[4]} 200.
+5. ${basePrompts[5]}
+6. ${basePrompts[6]}`;
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'system', content: process.env.SYSTEM_PROMPT! },
+      { role: 'user', content: prompt },
+    ],
+    stream: true,
+    max_tokens: 1024,
+    temperature: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  const stream = OpenAIStream(response);
+
+  return new StreamingTextResponse(stream);
+}
